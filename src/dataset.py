706d0aa59c2f450bff8d0ae6e7569807ab08debf
monteago30 <monteago30@gmail.com>
2025-03-18 15:47:54 -0400

    first commit

---
+++ src/dataset.py
@@ -0,0 +1,31 @@
+import os
+import sys
+from datetime import datetime
+from pathlib import Path
+from typing import Any, Dict
+
+
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    if b == 0:
+        return None
+    return a / b
+
+
+a = 10
+b = 5
+print(add(a, b))
+print(subtract(a, b))
+print(multiply(a, b))
+print(divide(a, b))
